b8f0b45ee116354ee148e4c70be58e2b49ab550f
Keshav Yadav <keshavl1247d@gmail.com>
2026-07-03 09:56:10 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
@@ -7,3 +9,5 @@ api.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+export default api;
